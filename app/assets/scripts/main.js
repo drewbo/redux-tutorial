a60dd19a4d4c1cc3b73fbc38df050abf0072c2d3
Drew Bollinger <drew@developmentseed.org>
2015-11-02 13:14:01 -0500

    Redo using react

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,35 +1,71 @@
 'use strict'
 var _ = require('lodash')
+var ReactRedux = require('react-redux')
+var React = require('react')
+var ReactDOM = require('react-dom')
 var config = require('./config')
 var store = require('./store')
 var actions = require('./actions')
 
-var render = function () {
-  var state = _.assign({}, store.getState())
+var Button = React.createClass({
+  render: function () {
+    return (
+      <div className={this.props.active ? 'active' : ''} onClick={this.props.toggleButtonActiveness}>
+        {this.props.buttonText}
+      </div>
+    )
+  }
+})
 
-  var siteCanvas = document.getElementById('site-canvas')
+var App = React.createClass({
 
-  // add a button with text and class supplied by "state"
-  // @ricardomestre would be upset at this non-semantic div but it was easier
-  // to style
-  siteCanvas.innerHTML = '<div class=' + (state.active ? 'active' : '') +
-  '>' + state.buttonText + '</div>'
+  componentDidMount: function () {
+    // change the button text every five seconds
+    var text = ['Hi! I\'m a button','Evil button','Happy button','Friendly button']
+    var self = this
+    setInterval(function () {
+      var randomText = text[Math.floor(Math.random() * text.length)]
+      self.props.updateButtonText(randomText)
+    }, 5000)
+  },
 
-  // add an event listener to the button
-  siteCanvas.firstChild.addEventListener('click', function () {
-    store.dispatch(actions.toggleButtonActiveness())
-  })
+  render: function () {
+    return (
+      <Button
+        active={this.props.active}
+        toggleButtonActiveness={this.props.toggleButtonActiveness}
+        buttonText={this.props.buttonText}
+      />
+    )
+  }
+})
+
+function mapStateToProps (state) {
+  return {
+    active: state.active,
+    buttonText: state.buttonText
+  }
 }
 
+function mapDispatchToProps (dispatch) {
+  return {
+    toggleButtonActiveness: function () {
+      dispatch(actions.toggleButtonActiveness())
+    },
+    updateButtonText: function (text) {
+      dispatch(actions.updateButtonText(text))
+    }
+  }
+}
+
+var ConnectedApp = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(App)
+
 // initial render
-render()
-
-// whenever the application state changes, call render again
-store.subscribe(render)
-
-// change the button text every five seconds
-var text = ['Hi! I\'m a button','Evil button','Happy button','Friendly button']
-setInterval(function () {
-  var randomText = text[Math.floor(Math.random() * text.length)]
-  store.dispatch(actions.updateButtonText(randomText))
-}, 5000)
+var rootElement = document.getElementById('site-canvas')
+var Provider = ReactRedux.Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedApp />
+  </Provider>,
+  rootElement
+)
